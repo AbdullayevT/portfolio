88cf3bc22a96082023d266dd2e6225b3fa4b01fd
Tapdig Abdullayev <abdullayevtapdiq2@gmail.com>
2023-09-04 16:26:40 +0400

    Updates

---
+++ assets/Dashboard-26b35ce9.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6bc867ea.js";function e(){return s.jsx("div",{className:"bg-white",children:s.jsx("h1",{className:"text-black h-screen",children:"Dashboard"})})}export{e as default};
--- assets/Dashboard-cd6a2f64.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-bc39f9e2.js";function e(){return s.jsx("div",{className:"bg-white",children:s.jsx("h1",{className:"text-black h-screen",children:"Dashboard"})})}export{e as default};
