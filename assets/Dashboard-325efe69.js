341084592b21e468ef7fe8acf18fb1a7b9c010ac
Tapdig Abdullayev <abdullayevtapdiq2@gmail.com>
2023-09-05 13:18:26 +0400

    Updates

---
+++ assets/Dashboard-325efe69.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-180e864a.js";function e(){return s.jsx("div",{className:"bg-white",children:s.jsx("h1",{className:"text-black h-screen",children:"Dashboard"})})}export{e as default};
