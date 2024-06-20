5238a24047bc1b960f3d4796cb135f853e890442
ATapdiq <abdullayevtapdiq2@gmail.com>
2024-06-20 13:57:45 +0400

    Updates

---
+++ assets/Dashboard-1276550f.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-9caf1165.js";function e(){return s.jsx("div",{className:"bg-white",children:s.jsx("h1",{className:"text-black h-screen",children:"Dashboard"})})}export{e as default};
